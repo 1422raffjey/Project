a0f5ae7ed584c051071db72508bf499c9b986088
raff jey <106816321+1422raffjey@users.noreply.github.com>
2022-06-03 13:45:30 +0100

    Add files via upload

---
+++ file.php
@@ -0,0 +1,16 @@
+<?php
+
+echo $_POST["firstname"];
+echo $_POST["lastname"];
+
+echo ("<form action=file.php method=post>") ;
+echo ("<input type=text name=firstname value= > <br />");
+echo ("<input type=hidden name=lastname value=><br />");
+echo ("<input type=text name=lastname value=><br />");
+echo ("<input type=text name=lastname value=><br />");
+echo ("<input type=text name=lastname value=><br />");
+echo ("<input type=submit  >");
+echo ("</form>");
+
+
+?>
---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+echo ("<form action=file.php method=post>") ;
+echo ("<input type=text name=firstname > <br />");
+echo ("<input type=text name=lastname ><br />");
+echo ("<input type=submit  >");
+echo ("</form>");
+
+
+?> 
+
+</body>
+</html>
